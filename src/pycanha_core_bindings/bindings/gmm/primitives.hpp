43f6b4bdaa8ef0337d9237d7052fbf5d0bc48bdd
Javier <javier.piqueras@upm.es>
2025-10-20 08:30:22 +0200

    starting v0.2

--- src/pycanha_core_bindings/bindings/gmm/primitives.hpp
+++ src/pycanha_core_bindings/bindings/gmm/primitives.hpp
@@ -1,10 +1,11 @@
 #pragma once
+#include <memory>
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <memory>
 
 #include "pycanha-core/gmm/primitives.hpp"
+#include "pycanha-core/parameters.hpp"
 
 namespace py = pybind11;
 using namespace pycanha::gmm;
@@ -23,7 +24,7 @@ void Primitive_b(py::module_ &m) {
 
 void Triangle_b(py::module_ &m) {
   py::class_<Triangle, Primitive, std::shared_ptr<Triangle>>(m, "Triangle")
-      .def(py::init<Point3D, Point3D, Point3D>())
+      .def(py::init<pycanha::Point3D, pycanha::Point3D, pycanha::Point3D>())
       .def_property("p1", &Triangle::get_p1, &Triangle::set_p1)
       .def_property("p2", &Triangle::get_p2, &Triangle::set_p2)
       .def_property("p3", &Triangle::get_p3, &Triangle::set_p3)
@@ -42,7 +43,7 @@ void Triangle_b(py::module_ &m) {
 
 void Rectangle_b(py::module_ &m) {
   py::class_<Rectangle, Primitive, std::shared_ptr<Rectangle>>(m, "Rectangle")
-      .def(py::init<Point3D, Point3D, Point3D>())
+      .def(py::init<pycanha::Point3D, pycanha::Point3D, pycanha::Point3D>())
       .def_property("p1", &Rectangle::get_p1, &Rectangle::set_p1)
       .def_property("p2", &Rectangle::get_p2, &Rectangle::set_p2)
       .def_property("p3", &Rectangle::get_p3, &Rectangle::set_p3)
@@ -62,7 +63,8 @@ void Rectangle_b(py::module_ &m) {
 void Quadrilateral_b(py::module_ &m) {
   py::class_<Quadrilateral, Primitive, std::shared_ptr<Quadrilateral>>(
       m, "Quadrilateral")
-      .def(py::init<Point3D, Point3D, Point3D, Point3D>())
+      .def(py::init<pycanha::Point3D, pycanha::Point3D, pycanha::Point3D,
+                    pycanha::Point3D>())
       .def_property("p1", &Quadrilateral::get_p1, &Quadrilateral::set_p1)
       .def_property("p2", &Quadrilateral::get_p2, &Quadrilateral::set_p2)
       .def_property("p3", &Quadrilateral::get_p3, &Quadrilateral::set_p3)
@@ -82,7 +84,8 @@ void Quadrilateral_b(py::module_ &m) {
 
 void Cylinder_b(py::module_ &m) {
   py::class_<Cylinder, Primitive, std::shared_ptr<Cylinder>>(m, "Cylinder")
-      .def(py::init<Point3D, Point3D, Point3D, double, double, double>())
+      .def(py::init<pycanha::Point3D, pycanha::Point3D, pycanha::Point3D,
+                    double, double, double>())
       .def_property("p1", &Cylinder::get_p1, &Cylinder::set_p1)
       .def_property("p2", &Cylinder::get_p2, &Cylinder::set_p2)
       .def_property("p3", &Cylinder::get_p3, &Cylinder::set_p3)
@@ -104,7 +107,8 @@ void Cylinder_b(py::module_ &m) {
 
 void Disc_b(py::module_ &m) {
   py::class_<Disc, Primitive, std::shared_ptr<Disc>>(m, "Disc")
-      .def(py::init<Point3D, Point3D, Point3D, double, double, double, double>())
+      .def(py::init<pycanha::Point3D, pycanha::Point3D, pycanha::Point3D,
+                    double, double, double, double>())
       .def_property("p1", &Disc::get_p1, &Disc::set_p1)
       .def_property("p2", &Disc::get_p2, &Disc::set_p2)
       .def_property("p3", &Disc::get_p3, &Disc::set_p3)
@@ -114,8 +118,7 @@ void Disc_b(py::module_ &m) {
                     &Disc::set_outer_radius)
       .def_property("start_angle", &Disc::get_start_angle,
                     &Disc::set_start_angle)
-      .def_property("end_angle", &Disc::get_end_angle,
-                    &Disc::set_end_angle)
+      .def_property("end_angle", &Disc::get_end_angle, &Disc::set_end_angle)
       .def("is_valid", &Disc::is_valid)
       .def("distance", &Disc::distance)
       .def("distance_jacobian_cutted_surface",
@@ -129,18 +132,16 @@ void Disc_b(py::module_ &m) {
 
 void Cone_b(py::module_ &m) {
   py::class_<Cone, Primitive, std::shared_ptr<Cone>>(m, "Cone")
-      .def(py::init<Point3D, Point3D, Point3D, double, double, double, double>())
+      .def(py::init<pycanha::Point3D, pycanha::Point3D, pycanha::Point3D,
+                    double, double, double, double>())
       .def_property("p1", &Cone::get_p1, &Cone::set_p1)
       .def_property("p2", &Cone::get_p2, &Cone::set_p2)
       .def_property("p3", &Cone::get_p3, &Cone::set_p3)
-      .def_property("radius1", &Cone::get_radius1,
-                    &Cone::set_radius1)
-      .def_property("radius2", &Cone::get_radius2,
-                    &Cone::set_radius2)
+      .def_property("radius1", &Cone::get_radius1, &Cone::set_radius1)
+      .def_property("radius2", &Cone::get_radius2, &Cone::set_radius2)
       .def_property("start_angle", &Cone::get_start_angle,
                     &Cone::set_start_angle)
-      .def_property("end_angle", &Cone::get_end_angle,
-                    &Cone::set_end_angle)
+      .def_property("end_angle", &Cone::get_end_angle, &Cone::set_end_angle)
       .def("is_valid", &Cone::is_valid)
       .def("distance", &Cone::distance)
       .def("distance_jacobian_cutted_surface",
@@ -154,20 +155,19 @@ void Cone_b(py::module_ &m) {
 
 void Sphere_b(py::module_ &m) {
   py::class_<Sphere, Primitive, std::shared_ptr<Sphere>>(m, "Sphere")
-      .def(py::init<Point3D, Point3D, Point3D, double, double, double, double, double>())
+      .def(py::init<pycanha::Point3D, pycanha::Point3D, pycanha::Point3D,
+                    double, double, double, double, double>())
       .def_property("p1", &Sphere::get_p1, &Sphere::set_p1)
       .def_property("p2", &Sphere::get_p2, &Sphere::set_p2)
       .def_property("p3", &Sphere::get_p3, &Sphere::set_p3)
-      .def_property("radius", &Sphere::get_radius,
-                    &Sphere::set_radius)
+      .def_property("radius", &Sphere::get_radius, &Sphere::set_radius)
       .def_property("base_truncation", &Sphere::get_base_truncation,
                     &Sphere::set_base_truncation)
       .def_property("apex_truncation", &Sphere::get_apex_truncation,
                     &Sphere::set_apex_truncation)
       .def_property("start_angle", &Sphere::get_start_angle,
                     &Sphere::set_start_angle)
-      .def_property("end_angle", &Sphere::get_end_angle,
-                    &Sphere::set_end_angle)
+      .def_property("end_angle", &Sphere::get_end_angle, &Sphere::set_end_angle)
       .def("is_valid", &Sphere::is_valid)
       .def("distance", &Sphere::distance)
       .def("distance_jacobian_cutted_surface",
--- src/pycanha_core_bindings/pycanha-core-wrapper.cpp
+++ src/pycanha_core_bindings/pycanha-core-wrapper.cpp
@@ -10,6 +10,9 @@
 #include "bindings/gmm/thermalmesh.hpp"
 #include "bindings/gmm/transformations.hpp"
 #include "bindings/gmm/trimesh.hpp"
+#include "bindings/tmm/couplings.hpp"
+#include "bindings/tmm/nodes.hpp"
+#include "bindings/tmm/thermalnetwork.hpp"
 #include "pycanha-core/utils/package_info.hpp"
 
 namespace py = pybind11;
@@ -18,6 +21,7 @@ using namespace pybind11::literals;  // for _a shorthand
 using namespace pycanha;
 
 using namespace gmm;
+using namespace pycanha::bindings::tmm;
 
 PYBIND11_MODULE(pycanha_core_cpp, m) {
   py::module gmm_submodule = m.def_submodule("gmm", "Geometry submodule");
@@ -47,6 +51,11 @@ PYBIND11_MODULE(pycanha_core_cpp, m) {
   TriMesh_b(gmm_submodule);
   TriMeshModel_b(gmm_submodule);
   primitive_meshers_b(gmm_submodule);
+
+  py::module tmm_submodule = m.def_submodule("tmm", "Thermal network submodule");
+  register_nodes(tmm_submodule);
+  register_couplings(tmm_submodule);
+  register_thermalnetwork(tmm_submodule);
 
   m.def("print_package_info", &print_package_info);
 }
